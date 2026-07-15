c1d7cdedb91fa6493c12d2ce69eb36f789094559
CatALoony_ <mc.huenchen321@gmail.com>
2026-07-15 06:32:17 +0200

    logging der installierten Server

--- src/events/clientReady/02console-log.js
+++ src/events/clientReady/02console-log.js
@@ -8,6 +8,10 @@ module.exports = {
       const guild = await client.guilds.cache.get(process.env.GUILD_ID);
       const targetUser = await guild.members.fetch(process.env.ADMIN_ID);
       targetUser.send(`Bot ist gestartet.`);
+      targetUser.send(`Der Bot ist laut API auf ${guilds.size} Server(n):`);
+      guilds.forEach((guild) => {
+        targetUser.send(`- ${guild.name} (ID: ${guild.id})`);
+      });
     } catch (error) {
       console.log(error);
     }
