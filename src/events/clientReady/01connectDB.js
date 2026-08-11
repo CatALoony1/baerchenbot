25d5b336ac41ea36f83cc8540eee2a8594e83190
Jonas <51858363+CatALoony1@users.noreply.github.com>
2026-08-11 16:38:59 +0200

    test connecting db

--- src/events/clientReady/01connectDB.js
+++ src/events/clientReady/01connectDB.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 module.exports = {
   once: true,
   run: async () => {
+    console.log('Connecting to DB...');
+    console.log(`MONGODB_URI: ${process.env.MONGODB_URI}`);
     try {
       mongoose.set('strictQuery', false);
       mongoose.set('debug', (collectionName, method, query, doc) => {
