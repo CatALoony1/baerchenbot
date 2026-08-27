e40e8490c88fa50401828dc1a9dcd586d9463754
Jonas <51858363+CatALoony1@users.noreply.github.com>
2026-08-27 16:10:02 +0200

    send start message to dev server

--- src/events/clientReady/03console-log.js
+++ src/events/clientReady/03console-log.js
@@ -6,16 +6,25 @@ module.exports = {
     console.log(`Logged in as ${client.user.tag}!`);
     try {
       let targetUser = undefined;
+      let targetChannel = undefined;
       const guilds = await client.guilds.cache;
       let message = `Bot ist gestartet.\nDer Bot ist laut API auf ${guilds.size} Server(n):\n`;
       await guilds.forEach(async (guild) => {
         if (!targetUser) {
           targetUser = await guild.members.fetch(process.env.ADMIN_ID);
         }
+        if (targetChannel) {
+          targetChannel =
+            guild.channels.cache.get('1527947035804696678') ||
+            (await guild.channels.fetch('1527947035804696678'));
+        }
         message = `${message}- ${guild.name} (ID: ${guild.id})`;
       });
       if (targetUser) {
-        targetUser.send(message);
+        await targetUser.send(message);
+      }
+      if (targetChannel) {
+        await targetChannel.send(message);
       }
     } catch (error) {
       console.log(error);
