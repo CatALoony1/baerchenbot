e350c3392ad20f999e2aac81682bfbd2e162bc7a
Jonas <51858363+CatALoony1@users.noreply.github.com>
2026-09-09 15:33:55 +0200

    neue Seite für Selfroles

--- src/web/website.js
+++ src/web/website.js
@@ -12,6 +12,7 @@ const logs = require('./routes/logs');
 const jobs = require('./routes/jobs');
 const channelselection = require('./routes/channelselection');
 const serverconfig = require('./routes/serverconfig');
+const roleSelect = require('./routes/role-select');
 const app = express();
 const port = 3003;
 const WebUser = require('../models/WebUser');
@@ -135,6 +136,7 @@ function startWebsite(client) {
   app.use('/channelselection', requireLogin, channelselection);
   app.use('/serverconfig', requireLogin, serverconfig);
   app.use('/games', requireLogin, games);
+  app.use('role-select', requireLogin, roleSelect);
 
   app.get(/(.*)/, (req, res) => {
     return res.redirect('/');
