e61eb5d3862aaa3b7ff86e94fe89eec61ade02a6
Jonas <51858363+CatALoony1@users.noreply.github.com>
2026-09-14 08:45:56 +0200

    role-select popup

--- src/web/routes/role-select.js
+++ src/web/routes/role-select.js
@@ -2,8 +2,28 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+  let rollen = [];
+  const selectedServerId = req.query.serverId || servers[0]?.id;
+  const allowedGuilds = req.session.guildIds;
+  if (allowedGuilds !== 'all') {
+    const allowedIds = allowedGuilds.split(',').map((id) => id.trim());
+    servers = servers.filter((server) => allowedIds.includes(server.id));
+  }
+  if (selectedServerId) {
+    const selectedGuild = client.guilds.cache.get(selectedServerId);
+    if (selectedGuild) {
+      rollen = selectedGuild.roles.cache.map((role) => ({
+        id: role.id,
+        name: role.name,
+      }));
+    }
+  }
   return res.render('role-select', {
-    guildIds: req.session.guildIds,
+    guildIds: allowedGuilds,
+    selectedServerId: selectedServerId,
+    servers: servers,
+    allSelNames: [],
+    rollen: rollen,
   });
 });
 
