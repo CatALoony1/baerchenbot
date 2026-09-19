d81e3e003bbb7ef64c04e32f8e9d29a37d854641
Jonas <51858363+CatALoony1@users.noreply.github.com>
2026-09-19 07:20:32 +0200

    add info message

--- src/web/routes/role-select.js
+++ src/web/routes/role-select.js
@@ -5,12 +5,27 @@ const { serverConfCache } = require('../../utils/data/cache');
 const printSelectMenu = require('../../utils/webFunctions/printSelectMenu');
 
 router.get('/', async (req, res) => {
+  let infoText = null;
   try {
     const client = req.discordClient;
     let servers = client.guilds.cache.map((guild) => ({
       id: guild.id,
       name: guild.name,
     }));
+    const info = req.query.info;
+    if (info) {
+      switch (info) {
+        case 1:
+          infoText = 'Die Nachricht des SelectMenus wurde in Discord gelöscht.';
+          break;
+        case 2:
+          infoText = 'Die Nachricht des SelectMenus wurde in Discord editiert.';
+          break;
+        case 3:
+          infoText = 'Die Nachricht des SelectMenus wurde in Discord gesendet.';
+          break;
+      }
+    }
     let rollen = [];
     const selectedServerId = req.query.serverId || servers[0]?.id;
     const allowedGuilds = req.session.guildIds;
@@ -60,6 +75,8 @@ router.get('/', async (req, res) => {
       rollen: rollen,
       allSelMenObj: allSelMenObj,
       error: null,
+      info: 0,
+      infoText: null,
     });
   } catch (error) {
     console.log(error);
@@ -104,6 +121,7 @@ router.post('/add', async (req, res) => {
 });
 
 router.post('/send', async (req, res) => {
+  let info = 0;
   try {
     const { serverId, selMenName } = req.body;
     const client = req.discordClient;
@@ -152,12 +170,14 @@ router.post('/send', async (req, res) => {
         selMenu.messageId,
       );
       await targetMessage.edit(messageContent);
+      info = 2;
     } else {
       const message = await targetChannel.send(messageContent);
       selMenu.messageId = message.id;
       await selMenu.save();
+      info = 3;
     }
-    return res.redirect(`/role-select?serverId=${serverId}`);
+    return res.redirect(`/role-select?serverId=${serverId}&info=${info}`);
   } catch (error) {
     console.log(error);
     return res.render('role-select', renderErrorTemplate(req, error.message));
@@ -165,9 +185,12 @@ router.post('/send', async (req, res) => {
 });
 
 router.post('/update', async (req, res) => {
+  let info = 0;
   try {
     // TODO
-    return res.redirect(`/role-select?serverId=${req.query.serverId}`);
+    return res.redirect(
+      `/role-select?serverId=${req.query.serverId}&info=${info}`,
+    );
   } catch (error) {
     console.log(error);
     return res.render('role-select', renderErrorTemplate(req, error.message));
@@ -176,6 +199,7 @@ router.post('/update', async (req, res) => {
 
 router.post('/delete', async (req, res) => {
   try {
+    let info = 0;
     const { serverId, selMenName } = req.body;
     const selMenu = await RoleSelectionRoles.findOne({
       guildId: serverId,
@@ -208,11 +232,12 @@ router.post('/delete', async (req, res) => {
           );
           if (targetMessage) {
             await targetMessage.delete();
+            info = 1;
           }
         }
       }
     }
-    return res.redirect(`/role-select?serverId=${serverId}`);
+    return res.redirect(`/role-select?serverId=${serverId}&info=${info}`);
   } catch (error) {
     console.log(error);
     return res.render('role-select', renderErrorTemplate(req, error.message));
@@ -228,6 +253,8 @@ function renderErrorTemplate(req, message) {
     rollen: [],
     allSelMenObj: new Set(),
     error: message,
+    info: 0,
+    infoText: null,
   };
 }
 
